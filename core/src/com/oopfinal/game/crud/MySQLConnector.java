b8155c4e4c5267abf9a5a441f0830b7f0e974c50
ana marie <anarie092223@gmail.com>
2024-05-27 11:07:53 +0800

    for jdbc. tell me if naay changes later for this

---
+++ core/src/com/oopfinal/game/crud/MySQLConnector.java
@@ -0,0 +1,23 @@
+package com.oopfinal.game.crud;
+
+import java.sql.*;
+
+public class MySQLConnector {
+    //access xampp using my account
+    private static final String URL = "";
+    private static final String USERNAME = "anais";
+    private static final String PASSWORD = "092223_aa";
+
+    public static Connection getConnection(){
+        Connection c = null;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }catch (ClassNotFoundException|SQLException e){
+            //throw new RuntimeException();
+            e.printStackTrace();
+        }
+        return c;
+    }
+}
---
+++ core/src/com/oopfinal/game/crud/SQLMethods.java
@@ -0,0 +1,7 @@
+package com.oopfinal.game.crud;
+
+public class SQLMethods {
+    //cannot use maven here kay wala ta ga-javafx
+    //will find solutions later
+    //idk where to place ang mga crud, so temporary location sa ni sila
+}
